72178c6b7317623a6ff3fff4bbda9b36a8341e85
Giovanni Dimeo <phinkie@GiovanDonato.Devp>
2019-05-16 16:22:12 +0200

    Test e fix sul preprocessor

--- Utils/PreProcessor.py
+++ Utils/PreProcessor.py
@@ -9,7 +9,7 @@ class processing: #ama cambie nom
     int2word = {}
 
     sentences = [] #list of sentences 
-    set_words = {} #set of words in each documents 
+    set_words = set() #set of words in each documents 
     vocab_size = 0
     data = [] #splitted word in a window [2]
 
@@ -18,15 +18,18 @@ class processing: #ama cambie nom
     
     def __init__(self,path):
         self.path = path
+        self.get_corpora()
 
     def get_corpora(self):
         os.chdir(self.path)
         g = glob.glob("*.*")
         for file in glob.glob("*.txt"):
+            print("Processing: " + file.title())
             with open(file) as f:
                 for line in f:
-                    self.set_words.update(line.split())
-                    raw_sentences = line.split('.')
+                    for ls in line.split():
+                        self.set_words.add(ls)
+                    raw_sentences = line.replace('\n','').split()
                     self.sentences.append(raw_sentences)
         self.vocab_size = len(self.set_words)
         #return a set of word and sentences in each documents 
@@ -36,6 +39,7 @@ class processing: #ama cambie nom
             for i,word in enumerate(self.set_words):
                 self.word2int[word] = i
                 self.int2word[i] = word
+    
 
     def split_inWindow(self,win_size = 2):
         WINDOW_SIZE = win_size
@@ -51,20 +55,38 @@ class processing: #ama cambie nom
         return temp
 
     def create_dataSet(self):
+        #print(self.sentences)
+        self.split_inWindow()
         for data_word in self.data:
             self.x_train.append(self.to_one_hot(self.word2int[data_word[0]], self.vocab_size))
             self.y_train.append(self.to_one_hot(self.word2int[data_word[1]], self.vocab_size))
         # convert them to numpy arrays
         self.x_train = np.asarray(self.x_train)
         self.y_train = np.asarray(self.y_train) 
-    
-    def save_data(self):
-        np.save("x_train",self.x_train)
-        np.save("y_train",self.y_train)
-        dizionari = []
-        dizionari.append(self.word2int)
-        dizionari.append(self.int2word)
+        self.save_data()
 
-        dict_file = open(self.path + "/dizionari.json", "a")
-        dict_file.write(json.dumps(dizionari))
-        dict_file.close()
+    def save_data(self):
+        try:
+            np.save("x_train",self.x_train)
+            np.save("y_train",self.y_train)
+            dizionari = []
+            dizionari.append(self.word2int)
+            dizionari.append(self.int2word)
+
+            dict_file = open(self.path + "/dizionari.json", "a")
+            dict_file.write(json.dumps(dizionari))
+            dict_file.close()
+            print("Salvataggio dati su: " + self.path)
+        except:
+            print("Errore nel salvataggio dei dati sul disco.")
+
+ 
+
+#testing processor 
+
+
+pre = processing("/home/phinkie/Scrivania/psychic-octo-system/data/")
+pre.build_dicts()
+pre.create_dataSet()
+
+
