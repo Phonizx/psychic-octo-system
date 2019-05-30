c004e3da77e34b28c0b44bd9bc2714962e5fb142
Giovanni Dimeo <phinkie@GiovanDonato.Devp>
2019-05-30 15:48:20 +0200

    Refactoring completato su octo

--- octo-system/octo.py
+++ octo-system/octo.py
@@ -10,16 +10,22 @@ import json
 import tflearn
 
 
-with open("/home/phinkie/Scrivania/psychic-octo-system/dataUtils/" + "words.json","r") as filewords:
-    fw =  json.load(filewords)
-    words = fw[0]
-    vocab_size = fw[1]
-    classes = fw[2]
-train_x = np.load("/home/phinkie/Scrivania/psychic-octo-system/dataUtils/" + "train_x.npy")
-train_y = np.load("/home/phinkie/Scrivania/psychic-octo-system/dataUtils/" + "train_y.npy")
+class Training:
+    def __init__(self,path):
+        self.path = path
+        self.load_trainingset()
+
+    def load_trainingset(self):
+        with open(self.path + "words.json","r") as filewords:
+            fw =  json.load(filewords)
+            self.words = fw[0]
+            self.vocab_size = fw[1]
+            self.classes = fw[2]
+        self.train_x = np.load(self.path + "train_x.npy")
+        self.train_y = np.load(self.path + "train_y.npy")
 
 
-def bow(sentence, words, show_details=False):
+    def bow(self,sentence, words, show_details=False):
         # tokenize the pattern
         sentence_words = sentence.replace("!","").split(' ')
          # bag of words
@@ -36,35 +42,47 @@ def bow(sentence, words, show_details=False):
 
         return bag
 
+    #linear classificator
+    def create_model(self):
+        tf.reset_default_graph()
+        # Input Layer
+        net = tflearn.input_data(shape=[None, len(self.train_x[0])])
+        #Hidden Layers con 20 hidden units
+        net = tflearn.fully_connected(net, 20)
+        net = tflearn.fully_connected(net, 20)
+        #Outpu Layer con fn. activation softmax
+        net = tflearn.fully_connected(net, len(self.train_y[0]), activation='softmax')
+        net = tflearn.regression(net)
+        model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+        return model
 
-def create_model():
-    tf.reset_default_graph()
-    # Input Layer
-    net = tflearn.input_data(shape=[None, len(train_x[0])])
-    #Hidden Layers con 20 hidden units
-    net = tflearn.fully_connected(net, 20)
-    net = tflearn.fully_connected(net, 20)
-    #Outpu Layer con fn. activation softmax
-    net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-    net = tflearn.regression(net)
-    model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
-    return model
+    def training(self):
+        model = self.create_model()
+        model.fit(self.train_x, self.train_y, n_epoch=1000, batch_size=50, show_metric=True)
+        print("Save model? ")
+        res = input()
+        if(res == 'y'):
+            model.save('model.tflearn')
+            print("Model stored in current path.")
+        
 
-#load_data("/home/phinkie/Scrivania/psychic-octo-system/dataUtils/")
-model = create_model()
-model.fit(train_x, train_y, n_epoch=1000, batch_size=50, show_metric=True)
-#model.save('model.tflearn')
 
-pred = model.predict(bow("mortal concession",words)) #2
+
+
+mod = Training("/home/phinkie/Scrivania/psychic-octo-system/dataUtils/")
+mod.training()
+
+
+#pred = model.predict(bow("mortal concession",words)) #2
 
 #sorted_array = np.sort(pred)
 #reverse_array = sorted_array[::-1]
 
-print(classes[np.argmax(pred)]) # + " Prob: \t" + str(pred))
+#print(classes[np.argmax(pred)]) # + " Prob: \t" + str(pred))
 #print(reverse_array[:3])
 
-pred = model.predict(bow("rit civil",words)) #3
-print(classes[np.argmax(pred)]) #+ " Prob: \t" + str(pred))
+#pred = model.predict(bow("rit civil",words)) #3
+#print(classes[np.argmax(pred)]) #+ " Prob: \t" + str(pred))
 
 #print(classes[np.argmax(model.predict(bow("talk to you yolo",words)))])
 #print(classes[np.argmax(model.predict(bow("can you make",words)))])
