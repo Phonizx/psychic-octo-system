4203431f74dad19aa608aca6b78fc215292925a2
Giovanni Dimeo <phinkie@GiovanDonato.Devp>
2019-06-04 22:36:23 +0200

    Fixed intern module, fixing tf

--- chatbot.py
+++ chatbot.py
@@ -12,7 +12,7 @@ from pymongo import MongoClient
 
 from flask import jsonify
 
-#from octosystem import predictor
+from octosystem import predictor
 from Utils import TextPreparation
 
 app = Flask(__name__,template_folder='templates')
