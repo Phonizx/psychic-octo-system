76d75c3db36229afe3974a9ca841398ecc5478ad
Giovanni Dimeo <phinkie@GiovanDonato.Devp>
2019-06-05 01:17:21 +0200

    Second full test

--- chatbot.py
+++ chatbot.py
@@ -20,11 +20,11 @@ app = Flask(__name__,template_folder='templates')
 
 pred = predictor.predictor("/app/dataUtils/")
 
-#cleaning = TextPreparation("/app/")
+cleaning = TextPreparation.TextPreparation("/app/")
 
-#pred.restore_model("/app/Models/0601 080/model.tflearn",554,240)
-#cleaning.load_stopWord("/app/dataUtils/stop_words.txt")
-#documenti = pred.get_documents()
+pred.restore_model("/app/Models/0601 080/model.tflearn",554,240)
+cleaning.load_stopWord("/app/dataUtils/stop_words.txt")
+documenti = pred.get_documents()
 
 store_ids = [0,0,0] #conserva gli id dei documenti correnti
 
